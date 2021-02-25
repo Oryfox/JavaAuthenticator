b6d3eaf82b496eb8a6a4921b292e50bffa4ca143
Oryfox <56044405+Oryfox@users.noreply.github.com>
2021-02-25 15:57:22 +0100

    Storage is saved using json format. Storage also holds all KeyItems (#1)

---
+++ src/KeyItem.java
@@ -0,0 +1,12 @@
+public class KeyItem {
+
+    String title; //Platform or whatever
+    String account ; //Username or E-Mail
+    String key; //Secret
+
+    public KeyItem(String title, String account, String key) {
+        this.title = title;
+        this.account = account;
+        this.key = key;
+    }
+}
